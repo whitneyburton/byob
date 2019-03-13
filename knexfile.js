df930504ac6f060f86386e41b4338d6ae94c0d78
Whitney Burton <whitneyburton9@gmail.com>
2019-03-12 20:47:55 -0600

    Add migrations to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,10 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/publications',
-    useNullAsDefault: true
+    connection: 'postgres://localhost/bouldering_areas',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './migrations'
+    },
   }
 };
