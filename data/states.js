92ca45642b70963d773ff556db15985f4fa76e17
Whitney Burton <whitneyburton9@gmail.com>
2019-03-12 19:57:48 -0600

    Add states dataset

---
+++ data/states.js
@@ -0,0 +1,57 @@
+const states = [
+  {
+    state: 'Washington',
+    stateTotal: 1037,
+    statePercentage: 15.8
+  },
+  {
+    state: 'Utah',
+    stateTotal: 2513,
+    statePercentage: 17.8
+  },
+  {
+    state: 'Colorado',
+    stateTotal: 7028,
+    statePercentage: 23.4
+  },
+  {
+    state: 'Texas',
+    stateTotal: 1332,
+    statePercentage: 47
+  },
+  {
+    state: 'Wyoming',
+    stateTotal: 638,
+    statePercentage: 11
+  },
+  {
+    state: 'Nevada',
+    stateTotal: 611,
+    statePercentage: 15.2
+  },
+  {
+    state: 'California',
+    stateTotal: 9284,
+    statePercentage: 29.1
+  },
+  {
+    state: 'Tennessee',
+    stateTotal: 519,
+    statePercentage: 24.7
+  },
+  {
+    state: 'Idaho',
+    stateTotal: 669,
+    statePercentage: 18.3
+  },
+  {
+    state: 'British Columbia',
+    stateTotal: 1986,
+    statePercentage: 34.1
+  },
+  {
+    state: 'New Mexico',
+    stateTotal: 823,
+    statePercentage: 14.7
+  }
+];
